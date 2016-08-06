72b8ee9a050c27fcdf0990cd999171653f2a1501
sxfans <sxfans@163.com>
2016-08-06 22:37:41 +0800

    新增代付类

---
+++ xpay-payment-biz/src/main/java/com/xpay/payment/biz/facade/AgentCollectFacadeImpl.java
@@ -0,0 +1,27 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2016 All Rights Reserved.
+ */
+package com.xpay.payment.biz.facade;
+
+import com.xpay.payment.common.facade.dto.AgentCollectFacade;
+import com.xpay.payment.common.facade.dto.agentcollect.AgentCollectRepDTO;
+import com.xpay.payment.common.facade.dto.agentcollect.AgentCollectReqDTO;
+import com.xpay.payment.common.facade.dto.agentcollect.QueryCollectRepDTO;
+import com.xpay.payment.common.facade.dto.agentcollect.QueryCollectReqDTO;
+
+/**
+ * @author qinshou
+ * @version $Id: AgentCollectFacadeImpl.java, v 0.1 16/8/6 下午9:27 sxfans Exp $
+ */
+public class AgentCollectFacadeImpl implements AgentCollectFacade {
+    @Override
+    public AgentCollectRepDTO pay(AgentCollectReqDTO agentCollectReqDTO) {
+        return null;
+    }
+
+    @Override
+    public QueryCollectRepDTO queryPay(QueryCollectReqDTO queryCollectReqDTO) {
+        return null;
+    }
+}
---
+++ xpay-payment-biz/src/main/java/com/xpay/payment/biz/facade/CustomerFacadeImpl.java
@@ -0,0 +1,21 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2016 All Rights Reserved.
+ */
+package com.xpay.payment.biz.facade;
+
+import com.xpay.payment.common.facade.dto.CustomerFacade;
+import com.xpay.payment.common.facade.dto.customer.CustomerRepDTO;
+import com.xpay.payment.common.facade.dto.customer.CustomerReqDTO;
+
+/**
+ * @author qinshou
+ * @version $Id: UserFacade.java, v 0.1 16/8/6 下午10:11 sxfans Exp $
+ */
+public class CustomerFacadeImpl implements CustomerFacade {
+
+    @Override
+    public CustomerRepDTO authRealName(CustomerReqDTO customerReqDTO) {
+        return null;
+    }
+}
