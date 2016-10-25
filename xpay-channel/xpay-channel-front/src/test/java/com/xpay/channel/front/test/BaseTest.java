2f31d07f485e06a094d9176eddc90edc57b453a4
sxfans <sxfans@163.com>
2016-10-25 16:16:04 +0800

    提交代码

--- xpay-channel/xpay-channel-front/src/test/java/com/xpay/channel/front/test/BaseTest.java
+++ xpay-channel/xpay-channel-front/src/test/java/com/xpay/channel/front/test/BaseTest.java
@@ -8,7 +8,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * Created by suxinxin on 16/2/5.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(locations = {"classpath*:spring/spring-quick-channel.xml"})
 @ContextConfiguration(locations = {"classpath*:spring/spring-channel.xml"})
 public class BaseTest {
 }
