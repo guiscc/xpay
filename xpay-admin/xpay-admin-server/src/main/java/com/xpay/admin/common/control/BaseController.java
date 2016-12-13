9f207a352dd44443f23ceed187c5f59d8417c8a8
sxfans <sxfans@163.com>
2016-12-13 13:26:44 +0800

    submit

---
+++ xpay-admin/xpay-admin-server/src/main/java/com/xpay/admin/common/control/BaseController.java
@@ -0,0 +1,111 @@
+package com.xpay.admin.common.control;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.github.miemiedev.mybatis.paginator.domain.PageBounds;
+import com.github.miemiedev.mybatis.paginator.domain.PageList;
+
+/**
+ *
+ * 
+ * @author TanDong
+ *
+ */
+public class BaseController {
+
+    /**
+     *
+     * @param pageSize
+     * @param currentPageNum
+     * @return
+     */
+    public PageBounds buildPageBounds(Integer pageSize, Integer currentPageNum,
+                                      Boolean isContainsTotalCount) {
+        if (null == pageSize) {
+            pageSize = 10;
+        }
+        if (null == currentPageNum) {
+            currentPageNum = 1;
+        }
+        if (null == isContainsTotalCount) {
+            isContainsTotalCount = true;
+        }
+        PageBounds pageBounds = new PageBounds();
+        pageBounds.setPage(currentPageNum);
+        pageBounds.setLimit(pageSize);
+        pageBounds.setContainsTotalCount(isContainsTotalCount);
+        return pageBounds;
+    }
+
+    /**
+     *
+     * @param pageSize
+     * @param currentPageNum
+     * @param ret
+     * @return
+     */
+    public Map<String, Object> buildResult(Integer pageSize, Integer currentPageNum, PageList<?> ret) {
+
+        if (null == pageSize) {
+            pageSize = 10;
+        }
+        if (null == currentPageNum) {
+            currentPageNum = 1;
+        }
+
+        Map<String, Object> result = new HashMap<String, Object>();
+        result.put("success", true);
+        result.put("rows", ret);
+        if (null != ret && null != ret.getPaginator()) {
+            result.put("total", ret.getPaginator().getTotalCount());
+        }
+        return result;
+    }
+
+    /**
+     *
+     * @param ret
+     * @return
+     */
+    public Map<String, Object> buildResult(Object ret) {
+        Map<String, Object> result = new HashMap<String, Object>();
+        result.put("success", true);
+        result.put("data", ret);
+        return result;
+    }
+
+    public Map<String, Object> buildResult(Integer pageSize, Integer currentPageNum,
+                                           PageList<?> ret, String income, String outcome) {
+        Map<String, Object> result = new HashMap<String, Object>();
+        result.put("success", true);
+        result.put("income", income);
+        result.put("outcome", outcome);
+        result.put("rows", ret);
+        result.put("total", ret.getPaginator().getTotalCount());
+        return result;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public Map<String, Object> buildResult() {
+        Map<String, Object> result = new HashMap<String, Object>();
+        result.put("success", true);
+        return result;
+    }
+
+    /**
+     * 
+     * @param errorMsg
+     * @return
+     */
+    public Map<String, Object> buildErrorResult(String errorMsg) {
+        Map<String, Object> result = new HashMap<String, Object>();
+        result.put("success", false);
+        result.put("errorMsg", errorMsg);
+        return result;
+    }
+
+}
---
+++ xpay-admin/xpay-admin-server/src/main/java/com/xpay/admin/common/exception/XpayAdminException.java
@@ -0,0 +1,54 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2016 All Rights Reserved.
+ */
+package com.xpay.admin.common.exception;
+
+import com.xpay.common.enums.EnumRtnResult;
+import com.xpay.common.exception.BaseException;
+
+/**
+ * @author qinshou
+ * @version $Id: XpayAdminException.java, v 0.1 16/12/13 上午11:41 sxfans Exp $
+ */
+public class XpayAdminException extends BaseException{
+
+    public XpayAdminException() {
+        super();
+    }
+
+    public XpayAdminException(String msg) {
+        super();
+    }
+
+    public XpayAdminException(EnumRtnResult rtnResult) {
+        super(rtnResult.name(), rtnResult.getMsg(), rtnResult.getStatus());
+        this.setRtnResult(rtnResult);
+        this.setErrorCode(rtnResult.name());
+        this.setErrorMsg(rtnResult.getMsg());
+        this.setRtnStatus(rtnResult.getStatus());
+    }
+
+    /**
+     * 返回信息
+     */
+    private EnumRtnResult rtnResult;
+
+    /**
+     * Getter method for property rtnResult.
+     *
+     * @return property value of rtnResult
+     **/
+    public EnumRtnResult getRtnResult() {
+        return rtnResult;
+    }
+
+    /**
+     * Setter method for property rtnResult.
+     *
+     * @param rtnResult value to be assigned to property rtnResult
+     **/
+    public void setRtnResult(EnumRtnResult rtnResult) {
+        this.rtnResult = rtnResult;
+    }
+}
